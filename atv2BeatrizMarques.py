b15d6ca0898cf24c690eee23ce49cd53ee7e94b7
bsibeatriz <beatrizmaarqueszo@gmail.com>
2023-02-16 21:04:36 -0300

    POO - atv1

---
+++ atv2BeatrizMarques.py
@@ -0,0 +1,7 @@
+'''
+1. Qual palavra-chave (keyword) você usaria para ter acesso às propriedades e métodos de uma classe
+estando dentro dela?
+R:  c) A palavra-chave self
+'''
+# Exercício de Codificação
+
