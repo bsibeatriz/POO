4eae97816fd626d0cffdd39537a1dfbb3b0090db
bsibeatriz <beatrizmaarqueszo@gmail.com>
2023-02-23 21:55:12 -0300

    :)

--- atv2BeatrizMarques.py
+++ atv2BeatrizMarques.py
@@ -5,3 +5,14 @@ R:  c) A palavra-chave self
 '''
 # Exercício de Codificação
 
+class Usuario():
+    primeiroNome = ""
+    segundoNome = ""
+
+    def hello(self):
+        return "Olá" + self.primeiroNome + " " + self.segundoNome
+
+usuario1 = Usuario()
+usuario1.primeiroNome = "Jonnie"
+usuario1.segundoNome = "Bravo"
+print(usuario1.hello())
